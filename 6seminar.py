0229d9960b21eca283aa076d433a1b88d3cd5823
Luda <millya92@gmail.com>
2023-07-09 21:04:05 +0300

    seminar6

--- 6seminar.py
+++ 6seminar.py
@@ -1,126 +1,88 @@
-# Добавьте в пакет, созданный на семинаре шахматный модуль. Внутри него напишите код, решающий задачу о 8 ферзях.
-# Известно, что на доске 8×8 можно расставить 8 ферзей так,
-# чтобы они не били друг друга. Вам дана расстановка 8 ферзей на доске,
-# определите, есть ли среди них пара бьющих друг друга.
-# Программа получает на вход восемь пар чисел, каждое число от 1 до 8 - координаты 8 ферзей.
-# Если ферзи не бьют друг друга верните истину, а если бьют - ложь.
+#В модуль с проверкой даты добавьте возможность запуска в терминале с передачей даты на проверку.
+
+from datetime import datetime
+from sys import argv
+
+
+def date_validate(date_text: str) -> bool:
+    try:
+        value = datetime.strptime(date_text, "%d.%m.%Y").date()
+        return True
+    except:
+        return False
+
+
+def _leap_info(date_text: str) -> bool:
+    year = int(date_text.split(".")[-1])
+    if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
+        return True
+    else:
+        return False
+
+
+if __name__ == '__main__':
+    date_validate(argv[2])
+
+#Добавьте в пакет, созданный на семинаре шахматный модуль. Внутри него напишите код, решающий задачу о 8 ферзях.
+#Известно, что на доске 8×8 можно расставить 8 ферзей так, чтобы они не били друг друга. 
+#Вам дана расстановка 8 ферзей на доске, определите, есть ли среди них пара бьющих друг друга. 
+#Программа получает на вход восемь пар чисел, каждое число от 1 до 8 - координаты 8 ферзей. 
+#Если ферзи не бьют друг друга верните истину, а если бьют - ложь.
+#Напишите функцию в шахматный модуль. Используйте генератор случайных чисел для случайной расстановки ферзей в задаче выше. 
+# Проверяйте различный случайные варианты и выведите 4 успешных расстановки.
 
 import random
+from copy import copy
 
 
-def make_desk(size: int) -> list[[int]]:
-    EMPTY = 0
-    return [[EMPTY for _ in range(size)] for _ in range(size)]
+class FerziBoard:
+    def __init__(self, length: int):
+        self.length = length
+        self.board = []
+        self.all_variants = []
+        self._place_next_ferz(0)
 
+    def _can_we_place(self, row: int, num: int):
+        for i, value in enumerate(self.board):
+            if value == num or (i - row) == (value - num) or (i - row) == (num - value):
+                return False
+        return True
 
-def show_desk(desk: list[[int]]) -> None:
-    print()
-    print(*desk, sep='\n')
-
-
-
-def feel_desk(desk: list[[int]], queens: list(())) -> list[[int]]:
-    '''
-    Функция заполняет доску ферзями.
-    1 - ферзь на поле, 0 - поле пустое
-    Возвращает доску в виде списка с расставленными фигурами
-    '''
-    BUSY = 1
-    for i in range(len(queens)):
-        desk[queens[i][0]][queens[i][1]] = BUSY
-    return desk
-
-
-def _check_turns(desk: list[[int]]) -> bool:
-    counter = 0
-    for col in desk:
-        for el in col:
-            if el == 1:
-                counter += 1
-        if counter > 1:
-            return False
+    def _place_next_ferz(self, row: int):
+        if row == self.length:
+            self.all_variants.append(copy(self.board))
         else:
-            counter = 0
-    return True
+            for col in range(self.length):
+                if self._can_we_place(row, col):
+                    self.board.append(col)
+                    self._place_next_ferz(row + 1)
+                    self.board.pop()
+
+    def check(self, board: list[int]):
+        return board in self.all_variants
 
 
-def _transparent(desk: list[[int]]) -> bool:
-    return [[*col] for col in zip(*desk)]
+def get_random_board(board=[0, 1, 2, 3, 4, 5, 6, 7]):
+    random.shuffle(board)
+    return board
 
 
-def _make_all_diagonal(desk: list[[int]]) -> bool:
-    n = len(desk)  # размер квадратной матрицы
-    all_diags = []  # здесь будут храниться все найденные
-    #     побочные диагонали
-    main, second = _check_main_diagonal(desk)
-    all_diags.append(main)
-    all_diags.append(second)
-    for distance in range(1, n):  # distance - расстояние от главной диагонали
-        sum_tl = n - 1 - distance  # сумма индексов (верхний-левый угол)
-        sum_br = n - 1 + distance  # сумма индексов (нижний-правый угол)
-        all_diags.extend([
-            [desk[i][sum_tl - i] for i in range(n - distance)],  # верх-лев диаг
-            [desk[i][sum_br - i] for i in range(distance, n)]  # ниж-прав диаг
-        ])
+if __name__ == '__main__':
+    length_of_board = 8
+    my_boards = FerziBoard(length_of_board)
+    print(my_boards.all_variants)
+    get_board_from_user = [0] * length_of_board
+    for count in range(length_of_board):
+        row, col = (int(i) - 1 for i in input(f"Введите пару координат № {count + 1} через пробел: ").split(" "))
+        get_board_from_user[col] = row
+    print(get_board_from_user)
+    print(my_boards.check(get_board_from_user))
+    count = 0
+    while count < 4:
+        random_board = get_random_board()
+        if my_boards.check(random_board):
+            print(random_board)
+            count += 1
 
-    return all_diags  # возвращаем все побочные диагонали
+    
 
-
-def is_beat(size: int, queens: list(())) -> bool:
-    desk = make_desk(size)
-    desk_with_queens = feel_desk(desk, queens)
-    if _check_turns(desk_with_queens):
-        rotate_desk = _transparent(desk_with_queens)
-        if _check_turns(rotate_desk):
-            diagonal_desk = _make_all_diagonal(desk_with_queens)
-            if _check_turns(diagonal_desk):
-                return True
-    return False
-
-
-def _check_main_diagonal(desk: list[[int]]) -> bool:
-    main_diag = [desk[i][i] for i in range(len(desk))]
-    secondary_diagonal = [desk[i][len(desk) - i - 1] for i in range(len(desk))]
-    return [main_diag, secondary_diagonal]
-
-
-def generate_queens_coordinates() -> list[()]:
-    LENGTH_COORDINATES = 8
-    coordinates = []
-    i = 0
-    while i <= LENGTH_COORDINATES - 1:
-        x, y = random.randint(0, 7), random.randint(0, 7)
-        if (x, y) not in coordinates:
-            coordinates.append((x, y))
-            i += 1
-    return coordinates
-
-
-def show_success() -> None:
-    SIZE = 8
-    count = 4
-    while count > 0:
-        coord_queens = generate_queens_coordinates()
-        result = is_beat(SIZE, coord_queens)
-        if result:
-            desk_with_queens = feel_desk(make_desk(SIZE), coord_queens)
-            show_desk(desk_with_queens)
-            print(result)
-            count -= 1
-
-
-if __name__ == "__main__":
-    desk_1 = make_desk(8)
-    desk_2 = make_desk(8)
-    queens = [(0, 0), (1, 1), (0, 2), (3, 3), (4, 4), (5, 5), (6, 6), (7, 7)]
-    desk_with_queens_1 = feel_desk(desk_1, queens)
-    show_desk(desk_with_queens_1)
-    print(is_beat(8, queens))
-    print()
-    queens_2 = [(0, 3), (1, 1), (2, 7), (3, 4), (4, 6), (5, 0), (6, 2), (7, 5)]
-    desk_with_queens_2 = feel_desk(desk_2, queens_2)
-    show_desk(desk_with_queens_2)
-    print(is_beat(8, queens_2))
-    # generate = generate_queens_coordinates()
-    # show_desk(generate)
-    show_success()
